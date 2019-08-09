a1f481b33af406daeed3c9092a9c18d19a386d64
Gabriel Davi <gdavi.gd@gmail.com>
2019-08-09 12:50:22 -0300

    Making the text robot

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const readline = require('readline-sync')
-
-function start(){
+const robots = {
+  text: require('./robots/text.js')
+}
+//Orquestrador, vai iniciar as funções necessárias e os robôs
+async function start(){
    const content = {}
 
    content.searchTerm = askAndReturnSearchTerm()
    content.prefix = askAndReturnPrefix()
 
+   await robots.text(content)
+
+   //Função para pegar a palavra do usuário
    function askAndReturnSearchTerm() {
      return readline.question("Type a wikipedia search term: ")
    }
 
+   //Função para pegar um prefixo que será o contexto da palavra
    function askAndReturnPrefix() {
      const prefixes = ['who is', 'what is', 'The history of']
      const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
