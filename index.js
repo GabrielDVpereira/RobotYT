2df703e9dcdf2934d58fc68cc7a99fbd4b214aeb
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-30 12:09:47 -0300

    Setting search image

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const robots = {
-  text: require('./robots/text.js'),
-  input: require('./robots/input.js'),
-  state: require('./robots/state.js')
-}
+  text: require("./robots/text.js"),
+  input: require("./robots/input.js"),
+  state: require("./robots/state.js"),
+  image: require("./robots/image.js"),
+};
 //Orquestrador, vai iniciar as funções necessárias e os robôs
-async function start(){
-
-   robots.input()
-   await robots.text()
-   console.dir(robots.state.load(), {depth: null});
+async function start() {
+  // robots.input();
+  // await robots.text();
+  await robots.image();
 }
 
-start()
+start();
